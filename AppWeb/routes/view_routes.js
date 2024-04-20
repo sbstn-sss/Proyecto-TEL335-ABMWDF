df99abc772ca9f7428ccdf088c3e138513d373df
sbstn-sss <sebastian.osses@sansano.usm.cl>
2024-04-19 20:06:01 -0400

    Subiendo cambios simplificacion de las vistas

--- AppWeb/routes/view_routes.js
+++ AppWeb/routes/view_routes.js
@@ -10,27 +10,38 @@ router
     .get('/', async (ctx, next) => {
         await ctx.render('inicio', {
             title: "Gestor de Canchas USM",
-            header: "Proyecto TEL335"
+            header: "Proyecto TEL335",
+            header_class: "" // es para colocarle un efecto al header: (ej: el de webosfire con class= nombre-integrantes)
         });
     })
     .get('/acerca', async (ctx, next) => {
         await ctx.render('acerca', {
             title: "Acerca del Proyecto",
-            header: "WEBOSFIRE"
+            header: "WEBOSFIRE",
+            header_class: ""
         });
     })
     .get('/integrantes', async (ctx, next) => {
         await ctx.render('integrantes', {
             title: "Integrantes del Grupo",
-            header: "WEBOSFIRE"
+            header: "WEBOSFIRE",
+            header_class: "nombre-grupo"
         }); 
     })
     .get('/cancha', async (ctx, next) => {
         await ctx.render('cancha', {
-            title: "Cancha"
+            title: "Cancha",
+            header: "Reserva de canchas",
+            header_class: ""
         });
     })
-
+    .get('/login', async (ctx, next) => {
+        await ctx.render('login', {
+            title: "Acceso USM",
+            header: "Login",
+            header_class: ""
+        });
+    })  
 
 
 
