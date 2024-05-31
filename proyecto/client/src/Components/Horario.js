23a95831b1d3ced587813c9f45176aeb5740d3f2
sbstn-sss <sebastian.osses@sansano.usm.cl>
2024-05-31 13:34:15 -0400

    subiendo calendario

--- proyecto/client/src/Components/Horario.js
+++ proyecto/client/src/Components/Horario.js
@@ -1,25 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import "./css/horario.css";
+import DatePicker from "react-multi-date-picker";
 
 export default function Horario() {
     const location = useLocation();
     const nombre = location.pathname.split("/")[2];
 
-    //console.log(nombre);
-
     const [cancha, setCancha] = useState([]);
-
-    //para los botones
-
     const [selectedButton, setSelectedButton] = useState({ day: '', index: -1 });
-
     const [reservas, setReservas] = useState([]);
-
     const [selectedWeek, setSelectedWeek] = useState('');
+    const [calendarWeek] = useState([]);
 
     useEffect(() => {
-        fetch(`http://127.0.0.1/api/canchas/${nombre}`, {
+
+        // Obtener la fecha del lunes de la semana actual
+        const today = new Date();
+        const diffToMonday = (today.getDay() === 0) ? 6 : today.getDay() - 1;
+        const mondayDate = new Date(today);
+        mondayDate.setDate(today.getDate() - diffToMonday);
+
+        setSelectedWeek(formatDate(mondayDate));
+
+        fetch(`http://127.0.0.1:8080/api/canchas/${nombre}`, {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
@@ -27,51 +31,37 @@ export default function Horario() {
         })
           .then(response => response.json())
           .then(data => {
-            // Actualiza el estado con los datos recibidos
-            setCancha(data.data.cancha); // Asigna todo el objeto data.data
-            //console.log(data.data.cancha);
+            setCancha(data.data.cancha);
           })
           .catch(error => {
             console.error('Error:', error);
           });
     }, [nombre]);
 
-    //console.log('cancha',cancha);
+    const fetchReservasForWeek = () => {
+        const url = `http://127.0.0.1:8080/api/reservas/${nombre}/semana/${selectedWeek}`;
+        console.log(url);
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.data.reservas);
+            setReservas(data.data.reservas);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    };
 
-    //busca las reservas en la bd
     useEffect(() => {
         if (selectedWeek) {
-            const startDate = new Date(selectedWeek);
-            const endDate = new Date(startDate);
-            endDate.setDate(startDate.getDate() + 4);
-
-            fetchReservasForWeek(startDate, endDate);
+            fetchReservasForWeek();
         }
     }, [selectedWeek]);
-
-    const fetchReservasForWeek = (startDate, endDate) => {
-        const promises = [];
-
-        for (let d = new Date(startDate); d <= endDate; d.setDate(d.getDate() + 1)) {
-            const formattedDate = formatDate(d);
-            const url = `http://127.0.0.1:8080/api/reservas/${nombre}/${formattedDate}`;
-            promises.push(fetch(url, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            }).then(response => response.json()));
-        }
-
-        Promise.all(promises)
-            .then(results => {
-                const allReservas = results.flatMap(result => result.data.reservas);
-                setReservas(allReservas);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-    };
 
     const formatDate = (date) => {
         const d = new Date(date);
@@ -85,14 +75,13 @@ export default function Horario() {
         setSelectedButton({ day, index });
     };
 
-    //revisa si esta reservado
     const isReserved = (day, index) => {
         const block = `${index * 2 + 1}-${index * 2 + 2}`;
         return reservas.some(reserva => reserva.dia_reservado === day && reserva.bloque === block);
     };
-    
+
     const renderButtons = (day) => {
-        return Array.from({ length: 8 }, (_, index) => (
+        return Array.from({ length: 10 }, (_, index) => (
             <div key={index}>
                 <button
                     className={isReserved(day, index) ? 'Red_Block' : (selectedButton.day === day && selectedButton.index === index ? 'Orange_Block' : 'Green_Block')}
@@ -104,13 +93,24 @@ export default function Horario() {
     };
 
     const renderTimeBlocks = () => {
-        return Array.from({ length: 8 }, (_, i) => (
+        return Array.from({ length: 10 }, (_, i) => (
             <div className="Block" key={i}>{i * 2 + 1}-{i * 2 + 2}</div>
         ));
     };
 
-    const handleWeekChange = (e) => {
-        setSelectedWeek(e.target.value);
+
+    const handleDateChangeTest = (dates) => {
+        if (dates && dates.length === 2) {
+            const startDate = new Date(dates[0]);
+            const startDay = startDate.getDay();
+            const diffToMonday = (startDay === 0) ? 1 : (startDay === 1) ? 0 : (8 - startDay);
+            const mondayDate = new Date(startDate);
+            mondayDate.setDate(startDate.getDate() + diffToMonday);
+
+            console.log(formatDate(mondayDate));
+
+            setSelectedWeek(formatDate(mondayDate));  // Actualiza el lunes de la semana seleccionada
+        }
     };
 
     return (
@@ -122,32 +122,55 @@ export default function Horario() {
                         <h1 className="font" style={{ marginLeft: '50px' }}>
                             {cancha.campus === "SJ" ? "Campus San Joaquin" : "Casa Central"}
                         </h1>
+                        
                         <div className="caja">
-                            <h1 className="font">Horario disponible Cancha</h1>
-                            <input type="week" onChange={handleWeekChange} />
+                            <h1 className="font">TEST</h1>
+                            <DatePicker
+                                value={calendarWeek}
+                                onChange={handleDateChangeTest}
+                                range
+                                weekPicker
+                                format="DD-MM-YYYY"
+                                mapDays={({ date }) => {
+                                    let props = {};
+                                    if (date.weekDay.index === 0 || date.weekDay.index === 6) {
+                                        props.disabled = true;
+                                    }
+                                    return props;
+                                }}
+                            />
+                            <h1>Lista de Reservas</h1>
+                            <ul>
+                                {reservas.map((reserva) => (
+                                <li key={reserva.id}>
+                                    {reserva.dia_reservado} - {reserva.bloque}
+                                </li>
+                                ))}
+                            </ul>
                         </div>
-                        <div className="flex">
+
+                        <div className="flex" style={{columnGap: '10px'}}>
                             <div className="flex_Block">
                                 Bloques de horario
                                 {renderTimeBlocks()}
                             </div>
-                            <div className="flex_inside">
+                            <div className="flex_inside" style={{width: '100px'}} >
                                 Lunes
                                 {renderButtons('Lunes')}
                             </div>
-                            <div className="flex_inside">
+                            <div className="flex_inside" style={{width: '100px'}} >
                                 Martes
                                 {renderButtons('Martes')}
                             </div>
-                            <div className="flex_inside">
+                            <div className="flex_inside" style={{width: '100px'}} >
                                 Miércoles
                                 {renderButtons('Miércoles')}
                             </div>
-                            <div className="flex_inside">
+                            <div className="flex_inside" style={{width: '100px'}} >
                                 Jueves
                                 {renderButtons('Jueves')}
                             </div>
-                            <div className="flex_inside">
+                            <div className="flex_inside" style={{width: '100px'}} >
                                 Viernes
                                 {renderButtons('Viernes')}
                             </div>
