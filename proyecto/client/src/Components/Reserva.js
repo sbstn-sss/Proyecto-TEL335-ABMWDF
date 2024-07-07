4dfd146971941364506da772715e9d87b35fbe77
yzh02U <83672268+yzh02U@users.noreply.github.com>
2024-07-07 01:03:19 -0400

    Update Reserva.js

--- proyecto/client/src/Components/Reserva.js
+++ proyecto/client/src/Components/Reserva.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useCookies } from 'react-cookie';
 import './css/reserva.css';
-import { useCookies } from 'react-cookie'; 
 
 export default function Reserva() {
-<<<<<<< Updated upstream
   const [reservas, setReservas] = useState([]);
   const [cookies] = useCookies(['jwt']);
 
@@ -71,60 +69,6 @@ export default function Reserva() {
       )) : (
         <p>No hay reservas disponibles.</p>
       )}
-=======
-  const [cookies, setCookie, removeCookie] = useCookies(['jwt', 'id_usuario', 'email', 'nombre', 'rol', 'tipo_usuario']);
-
-  const getReservas = () => {
-
-
-    fetch(`http://127.0.0.1:8080/api/users/reservas/mine`, {
-      method: 'GET',
-      credentials: 'include',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-   })
-    .then(response => response.json())
-    .then(data => {
-
-      var cont = 0;
-        data.forEach(datos => {
-          
-          cont += 1;
-          <div class="header">
-            <h1 class="font">Reserva {cont}:</h1>
-            <h1 class="Confirm">Por confirmar</h1>
-            <h1 class="font">{datos.data.reservas.fecha}</h1>
-
-            <button class="Cancel">Cancelar</button>
-
-            <div class="Flex">
-
-
-            </div>
-          </div>
-
-        });
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-   
-
-  }
-
-
-  return (
-    <div>
-
-
-      <div class="caja">
-        <h1 class="font">Estado</h1>
-        <div>
-            {getReservas()}
-         </div>  
-      </div>
->>>>>>> Stashed changes
     </div>
   );
 }
